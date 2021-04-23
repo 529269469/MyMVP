118a0e10ffcb7acd99b6bc83e93f68b3ae184ef3
yourWYW <529269469@qq.com>
2021-04-23 10:25:48 +0800

    接入阿里移动devops

--- app/src/main/java/com/example/mymvp/MyApplication.java
+++ app/src/main/java/com/example/mymvp/MyApplication.java
@@ -32,14 +32,14 @@ public class MyApplication extends Application {
     private void initUpdate() {
         //以下引号部分需要客户根据自己的应用进行配置（）
         Config config = new Config();
-        config.group = "appkey" + "@android";   //填写appkey
+        config.group = "333424457" + "@android";   //填写appkey
         config.ttid = "渠道号";    //渠道号
         config.isOutApk = false;
-        config.appName = "appname"; //app name
+        config.appName = "workermvp"; //app name
         UpdateRuntime.init(this, config.ttid, config.appName, config.group);
-        ApkUpdater apkupdate = new ApkUpdater(getApplicationContext(), "appkey", "appsecret", config.group, "渠道号", config.ttid);
+        ApkUpdater apkupdate = new ApkUpdater(getApplicationContext(), "333424457", "af55e77782e648e69bda49943dce5ee2", config.group, "渠道号", config.ttid);
         UpdateAdapter updateAdapter = new UpdateAdapter();
-        UpdateDataSource.getInstance().init(this, config.group, config.ttid, config.isOutApk, "appkey", "appsecret", "渠道号", updateAdapter);
+        UpdateDataSource.getInstance().init(this, config.group, config.ttid, config.isOutApk, "333424457", "af55e77782e648e69bda49943dce5ee2", "渠道号", updateAdapter);
         UpdateDataSource.getInstance().startUpdate(false);
     }
 
