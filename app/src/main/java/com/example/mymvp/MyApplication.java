2e89ec391a1f40c4ea5939f8c71bbe140264d581
yourWYW <529269469@qq.com>
2021-04-27 08:36:20 +0800

    接入IM

--- app/src/main/java/com/example/mymvp/MyApplication.java
+++ app/src/main/java/com/example/mymvp/MyApplication.java
@@ -8,11 +8,9 @@ import com.taobao.update.apk.ApkUpdater;
 import com.taobao.update.common.Config;
 import com.taobao.update.common.framework.UpdateRuntime;
 import com.taobao.update.datasource.UpdateDataSource;
+import com.tencent.imsdk.v2.V2TIMManager;
 import com.tencent.imsdk.v2.V2TIMSDKConfig;
-import com.tencent.qcloud.tim.uikit.TUIKit;
-import com.tencent.qcloud.tim.uikit.config.CustomFaceConfig;
-import com.tencent.qcloud.tim.uikit.config.GeneralConfig;
-import com.tencent.qcloud.tim.uikit.config.TUIKitConfigs;
+import com.tencent.imsdk.v2.V2TIMSDKListener;
 
 
 public class MyApplication extends Application {
@@ -33,12 +31,31 @@ public class MyApplication extends Application {
     }
 
     private void initTUIKit() {
-        // 配置 Config，请按需配置
-        TUIKitConfigs configs = TUIKit.getConfigs();
-        configs.setSdkConfig(new V2TIMSDKConfig());
-        configs.setCustomFaceConfig(new CustomFaceConfig());
-        configs.setGeneralConfig(new GeneralConfig());
-        TUIKit.init(this, 123, configs);
+        // 1. 从 IM 控制台获取应用 SDKAppID，详情请参考 SDKAppID。
+        // 2. 初始化 config 对象
+        V2TIMSDKConfig config = new V2TIMSDKConfig();
+        // 3. 指定 log 输出级别，详情请参考 SDKConfig。
+        config.setLogLevel(V2TIMSDKConfig.V2TIM_LOG_INFO);
+        // 4. 初始化 SDK 并设置 V2TIMSDKListener 的监听对象。
+        // initSDK 后 SDK 会自动连接网络，网络连接状态可以在 V2TIMSDKListener 回调里面监听。
+        V2TIMManager.getInstance().initSDK(this, 123, config, new V2TIMSDKListener() {
+            // 5. 监听 V2TIMSDKListener 回调
+            @Override
+            public void onConnecting() {
+                // 正在连接到腾讯云服务器
+            }
+
+            @Override
+            public void onConnectSuccess() {
+                // 已经成功连接到腾讯云服务器
+            }
+
+            @Override
+            public void onConnectFailed(int code, String error) {
+                // 连接腾讯云服务器失败
+            }
+        });
+
     }
 
     public static MyApplication getInstances() {
